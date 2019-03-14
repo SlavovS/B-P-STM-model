6ee9150eaa25d46e35d1a68478cfa96f7b764f32
SlavovS <42062794+SlavovS@users.noreply.github.com>
2019-03-14 09:47:40 +0200

    Add files via upload

---
+++ BP_STM_parameters.py
@@ -0,0 +1,10 @@
+# Parameters for Botvinick & Plaut model
+
+
+length_of_list = 3
+training_iterations = 4
+test_iterations = 2
+
+letters_3 = [0,0,1]
+
+letters_26 = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1]
