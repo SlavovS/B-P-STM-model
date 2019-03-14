15dcd29bf33feb0cffcd163405137097fedf100c
SlavovS <42062794+SlavovS@users.noreply.github.com>
2019-03-14 20:05:30 +0200

    Updated generation of input and output
    Fixed problems with generating matching input and putput

--- BP_STM_model.py
+++ BP_STM_model.py
@@ -17,13 +17,13 @@ from BP_STM_parameters import *
 
 # Generating a letter (localist representation), and an "off" recall cue.
 def ran_letter_gen():
-    letters = np.random.permutation(letters_3)   
+    letters = np.random.permutation(letters_26)   
     return np.array(np.append(letters, [0])).astype("float64")
 
 
 # Generates an "on" recall cue attached to the letters
 def recall_cue():
-    return np.array(np.append(np.zeros(len(letters_3)), [1]))#np.array([0, 0, 0, 1])
+    return np.array(np.append(np.zeros(len(letters_26)), [1]))#np.array([0, 0, 0, 1])
 
 
 
@@ -63,7 +63,7 @@ def final_input():
                                             # cues as there are letters
     
     final_input_letters = np.array(np.append(input_letters, recall_cue()))
-    final_input_letters = np.reshape(final_input_letters, (2*length_of_list+1, len(letters_3) + 1))
+    final_input_letters = np.reshape(final_input_letters, (2*length_of_list+1, len(letters_26) + 1))
     
     final_output_letters = np.array(output_letters[0:-length_of_list]+output_letters[0:-length_of_list+1])
     
@@ -74,9 +74,10 @@ def final_input():
 
 X = []
 y = []
-for letter in range(0, training_iterations):
-    X.append(final_input()[0])
-    y.append(final_input()[1])
+for letter in range(0, training_examples):
+    training_array = final_input()
+    X.append(training_array[0])
+    y.append(training_array[1])
     
 X = np.array(X, dtype=float)
 y = np.array(y, dtype=float)
@@ -87,12 +88,13 @@ y = np.array(y, dtype=float)
 
 X_test = []
 y_test = []
-for letter in range(0, test_iterations):
-    X_test.append(final_input()[0])
-    y_test.append(final_input()[1])
+for letter in range(0, test_examples):
+    test_array = final_input()
+    X_test.append(test_array[0])
+    y_test.append(test_array[1])
     
-X_test = np.array(X, dtype=float)
-y_test = np.array(y, dtype=float)
+X_test = np.array(X_test, dtype=float)
+y_test = np.array(y_test, dtype=float)
 
 
 
@@ -104,7 +106,7 @@ BP_model = Sequential()
 BP_model.add(
         LSTM(
                 units = 50, 
-                input_shape=(None, len(letters_3) + 1), 
+                input_shape=(None, len(letters_26) + 1), 
                 return_sequences=True
         )
     )
@@ -112,7 +114,7 @@ BP_model.add(
 BP_model.add(
         TimeDistributed(
                 Dense(
-                        units = len(letters_3) + 1,
+                        units = len(letters_26) + 1,
                         activation="softmax"
                 )
             )
@@ -123,6 +125,53 @@ BP_model.fit(
         batch_size=1, 
         nb_epoch=500, 
         verbose=2, 
-        validation_data=(final_input_letters, final_output_letters)
+        validation_data=(X_test, y_test)
     )
-BP_model.save('B&P_model.h5')
+BP_model.save('Chernova_B&P_model.h5')
+
+
+  #Reloading and testing the model:
+      #Epoch 500/500  70 training examples, 30 test examples:
+      # loss: 5.9517e-05 - acc: 1.0000 - val_loss: 1.9525 - val_acc: 0.7286
+
+from keras.models import load_model
+ISR_model = load_model('Chernova_B&P_model.h5')
+
+
+#Epoch 500/500   with 10 training examples
+#0s - loss: 0.0462 - acc: 0.9714 - val_loss: 0.0459 - val_acc: 0.9714
+check = np.array([[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                  [0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                  [0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                  [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+                  [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+                  [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+                  [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1]], 
+            dtype=float).reshape(1, 2*length_of_list+1, len(letters_26) + 1)
+
+#check = np.array([[0,0,1,0],
+#                  [0,1,0,0],
+#                  [1,0,0,0],
+#                  [0,0,0,1],
+#                  [0,0,0,1],
+#                  [0,0,0,1],
+#                  [0,0,0,1]],
+#            dtype=float).reshape(1, 2*length_of_list+1, len(letters_3) + 1)
+
+
+prediction = ISR_model.predict(check)
+print(prediction[0][3])
+print(prediction[0][4])
+print(prediction[0][5])
+
+# Epoch 500/500  with 100 training examples
+# 1s - loss: 0.2319 - acc: 0.8671 - val_loss: 0.2168 - val_acc: 0.8900
+
+
+# Checking the cosine similarity
+from scipy import spatial
+
+dataSetI = check[0][0]
+dataSetII = prediction[0][3]
+result = 1 - spatial.distance.cosine(dataSetI, dataSetII)
+print("The cosine similarity between a letter in the input and the model's output is: {} ".format(result))
---
+++ BP_STM_parameters.py
@@ -0,0 +1,10 @@
+# Parameters for Botvinick & Plaut model
+
+
+length_of_list = 3
+training_examples = 70
+test_examples = 30
+
+letters_3 = [0,0,1]
+
+letters_26 = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1]
